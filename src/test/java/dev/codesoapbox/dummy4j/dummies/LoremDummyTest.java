886f66b26e982ffb46897e32ef7708a1dd8552d6
little-pinecone <marta.szymek.mail@gmail.com>
2020-07-21 15:06:11 +0200

    Add unit tests to LoremDummy.

--- src/test/java/dev/codesoapbox/dummy4j/dummies/LoremDummyTest.java
+++ src/test/java/dev/codesoapbox/dummy4j/dummies/LoremDummyTest.java
@@ -1,74 +1,161 @@
 package dev.codesoapbox.dummy4j.dummies;
 
 import dev.codesoapbox.dummy4j.Dummy4j;
+import dev.codesoapbox.dummy4j.ExpressionResolver;
+import dev.codesoapbox.dummy4j.RandomService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class LoremDummyTest {
 
+    private static final int ENDING_CHAR_PROBABILITY = 15;
+    private static final int END_WITH_DOT = 10;
+    private static final int END_WITH_OTHER = 1;
+
+    @Mock
     private Dummy4j dummy4j;
+
+    @Mock
+    private ExpressionResolver expressionResolver;
+
+    @Mock
+    private RandomService randomService;
+
+    private LoremDummy loremDummy;
 
     @BeforeEach
     void setUp() {
-        dummy4j = new Dummy4j();
+        loremDummy = new LoremDummy(dummy4j);
     }
 
     @Test
-    void characters() {
-        String value = dummy4j.lorem().characters(10);
-        assertNotNull(value);
-        assertFalse(value.isEmpty());
-        assertEquals(10, value.length());
+    void shouldReturnCharacter() {
+        mockExpressionResolver();
+        when(expressionResolver.resolveKey("lorem.characters"))
+                .thenReturn("a");
+        assertEquals("a", loremDummy.character());
+    }
+
+    private void mockExpressionResolver() {
+        when(dummy4j.expressionResolver())
+                .thenReturn(expressionResolver);
     }
 
     @Test
-    void character() {
-        String value = dummy4j.lorem().character();
-        assertNotNull(value);
-        assertFalse(value.isEmpty());
+    void shouldReturnStringOfCharacters() {
+        mockExpressionResolver();
+        when(expressionResolver.resolveKey("lorem.characters"))
+                .thenReturn("a");
+        assertEquals("aaa", loremDummy.characters(3));
     }
 
     @Test
-    void sentence() {
-        String value = dummy4j.lorem().sentence();
-        assertNotNull(value);
-        assertFalse(value.isEmpty());
+    void shouldReturnSentenceEndingWithDot() {
+        mockRandomService();
+        when(randomService.nextInt(3, 10))
+                .thenReturn(4);
+        when(dummy4j.random().nextInt(ENDING_CHAR_PROBABILITY))
+                .thenReturn(END_WITH_DOT);
+
+        mockExpressionResolver();
+        when(dummy4j.expressionResolver().resolveKey("lorem.word"))
+                .thenReturn("lorem");
+
+        assertEquals("Lorem lorem lorem lorem.", loremDummy.sentence());
+    }
+
+    private void mockRandomService() {
+        when(dummy4j.random())
+                .thenReturn(randomService);
     }
 
     @Test
-    void sentenceWithNumberOfWords() {
-        String value = dummy4j.lorem().sentence(10);
-        assertNotNull(value);
-        assertFalse(value.isEmpty());
+    void shouldReturnSentenceEndingWithExclamationMark() {
+        mockRandomService();
+        when(randomService.nextInt(3, 10))
+                .thenReturn(4);
+        when(dummy4j.random().nextInt(ENDING_CHAR_PROBABILITY))
+                .thenReturn(END_WITH_OTHER);
+
+        mockExpressionResolver();
+        when(dummy4j.expressionResolver().resolveKey("lorem.word"))
+                .thenReturn("lorem");
+        when(dummy4j.expressionResolver().resolveKey("lorem.additional_sentence_ending_punctuation"))
+                .thenReturn("!");
+
+        assertEquals("Lorem lorem lorem lorem!", loremDummy.sentence());
     }
 
     @Test
-    void sentenceWithMinAndMaxWords() {
-        String value = dummy4j.lorem().sentence(5, 10);
-        assertNotNull(value);
-        assertFalse(value.isEmpty());
+    void shouldReturnSentenceWithGivenNumberOfWords() {
+        mockRandomService();
+        when(dummy4j.random().nextInt(ENDING_CHAR_PROBABILITY))
+                .thenReturn(END_WITH_DOT);
+
+        mockExpressionResolver();
+        when(dummy4j.expressionResolver().resolveKey("lorem.word"))
+                .thenReturn("lorem");
+
+        assertEquals("Lorem lorem.", loremDummy.sentence(2));
     }
 
     @Test
-    void word() {
-        String value = dummy4j.lorem().word();
-        assertNotNull(value);
-        assertFalse(value.isEmpty());
+    void shouldReturnSentenceOfWordCountBetweenMinAndMax() {
+        mockRandomService();
+        when(randomService.nextInt(1, 10))
+                .thenReturn(1);
+        when(dummy4j.random().nextInt(ENDING_CHAR_PROBABILITY))
+                .thenReturn(END_WITH_DOT);
+
+        mockExpressionResolver();
+        when(dummy4j.expressionResolver().resolveKey("lorem.word"))
+                .thenReturn("lorem");
+        assertEquals("Lorem.", loremDummy.sentence(1, 10));
     }
 
     @Test
-    void paragraph() {
-        String value = dummy4j.lorem().paragraph();
-        assertNotNull(value);
-        assertFalse(value.isEmpty());
+    void shouldReturnWord() {
+        mockExpressionResolver();
+        when(expressionResolver.resolveKey("lorem.word"))
+                .thenReturn("lorem");
+
+        assertEquals("lorem", loremDummy.word());
     }
 
     @Test
-    void paragraphWithNumberOfSentences() {
-        String value = dummy4j.lorem().paragraph(10);
-        assertNotNull(value);
-        assertFalse(value.isEmpty());
+    void shouldReturnParagraph() {
+        mockRandomService();
+        when(randomService.nextInt(3, 10))
+                .thenReturn(3);
+        when(dummy4j.random().nextInt(ENDING_CHAR_PROBABILITY))
+                .thenReturn(END_WITH_DOT);
+
+        mockExpressionResolver();
+        when(dummy4j.expressionResolver().resolveKey("lorem.word"))
+                .thenReturn("lorem");
+
+        assertEquals("Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem.", loremDummy.paragraph());
+    }
+
+    @Test
+    void shouldReturnParagraphWithGivenNumberOfSentences() {
+        mockRandomService();
+        when(randomService.nextInt(3, 10))
+                .thenReturn(3);
+        when(dummy4j.random().nextInt(ENDING_CHAR_PROBABILITY))
+                .thenReturn(END_WITH_DOT);
+
+        mockExpressionResolver();
+        when(dummy4j.expressionResolver().resolveKey("lorem.word"))
+                .thenReturn("lorem");
+
+        assertEquals("Lorem lorem lorem. Lorem lorem lorem.", loremDummy.paragraph(2));
     }
 }
