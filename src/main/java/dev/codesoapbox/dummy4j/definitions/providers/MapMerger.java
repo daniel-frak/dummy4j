538c8583dcedbc43b5db1994b55f7a0191f5b8b7
Daniel Frąk <dan.frak@gmail.com>
2020-07-22 16:05:21 +0200

    Refactor MapMerger and make it not mutate original maps

--- src/main/java/dev/codesoapbox/dummy4j/definitions/providers/MapMerger.java
+++ src/main/java/dev/codesoapbox/dummy4j/definitions/providers/MapMerger.java
@@ -3,6 +3,7 @@ package dev.codesoapbox.dummy4j.definitions.providers;
 import java.util.*;
 import java.util.stream.Stream;
 
+import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 
 /**
@@ -17,77 +18,81 @@ public class MapMerger {
      * @return a merged Map
      */
     public Map<String, Object> merge(List<Map<String, Object>> definitionMaps) {
-        Map<String, Object> mergedMap = new HashMap<>();
+        Map<String, Object> target = new HashMap<>();
 
-        definitionMaps.forEach(otherMap -> mergeMap(mergedMap, otherMap));
-        return mergedMap;
+        definitionMaps.forEach(otherMap -> mergeMap(target, otherMap));
+        return target;
     }
 
-    private void mergeMap(Map<String, Object> mergedMap, Map<String, Object> otherMap) {
+    private Map<String, Object> mergeMap(Map<String, Object> targetMap, Map<String, Object> otherMap) {
         otherMap.forEach((key, value) ->
-                mergeMapKey(mergedMap, otherMap, key, value));
+                mergeMapKey(targetMap, otherMap, key, value));
+        return targetMap;
     }
 
-    private void mergeMapKey(Map<String, Object> mergedMap, Map<String, Object> otherMap, String key, Object value) {
-        if (!mergedMap.containsKey(key)) {
-            mergedMap.put(key, value);
+    @SuppressWarnings("unchecked")
+    private void mergeMapKey(Map<String, Object> targetMap, Map<String, Object> otherMap, String key, Object value) {
+        if (!targetMap.containsKey(key)) {
+            targetMap.put(key, value);
             return;
         }
 
-        if (mergedMap.get(key) instanceof String) {
-            if (otherMap.get(key) instanceof String) {
-                mergedMap.put(key, Arrays.asList(mergedMap.get(key), otherMap.get(key)));
-            } else { // Assuming OtherMap key is a Collection
-                @SuppressWarnings("unchecked")
-                List<String> mergedList = mergeCollectionWithElement(
-                        (String) mergedMap.get(key),
-                        (Collection<String>) otherMap.get(key));
-                mergedMap.put(key, mergedList);
-            }
-        } else if (mergedMap.get(key) instanceof Map) {
-            Map<String, Object> mergedValue = mergeMapKeyAsMap(mergedMap, otherMap, key);
-            mergedMap.put(key, mergedValue);
-        } else { // Assuming MergedMap key is a Collection
-            if (otherMap.get(key) instanceof String) {
-                @SuppressWarnings("unchecked")
-                List<String> mergedList = mergeElementWithCollection(
-                        (String) otherMap.get(key),
-                        (Collection<String>) mergedMap.get(key));
-                mergedMap.put(key, mergedList);
-            } else { // Assuming OtherMap key is a Collection
-                Object mergedValue = mergeMapKeyAsList(mergedMap, otherMap, key);
-                mergedMap.put(key, mergedValue);
-            }
+        Object targetValue = targetMap.get(key);
+        Object otherValue = otherMap.get(key);
+
+        if (targetValue instanceof String) {
+            final List<?> result = mergeStringWithObject((String) targetValue, otherValue);
+            targetMap.put(key, result);
+        } else if (targetValue instanceof Map) {
+            Map<String, Object> result = mergeMap(
+                    new HashMap<>((Map<String, Object>) targetValue), (Map<String, Object>) otherValue);
+            targetMap.put(key, result);
+        } else {
+            // Assuming targetValue is a Collection of Strings
+            Collection<?> result = mergeCollectionWithOtherObject((Collection<String>) targetValue, otherValue);
+            targetMap.put(key, result);
         }
     }
 
-    private List<String> mergeCollectionWithElement(String string, Collection<String> collection) {
+    @SuppressWarnings("unchecked")
+    private Collection<?> mergeCollectionWithOtherObject(Collection<String> targetValue, Object otherValue) {
+        if (otherValue instanceof String) {
+            return addToEndOfCollection( targetValue, (String) otherValue);
+        } else {
+            // Assuming otherValue is a Collection of Strings
+            return mergeListsOfStrings(targetValue,(Collection<String>) otherValue);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<?> mergeStringWithObject(String targetValue, Object otherValue) {
+        if (otherValue instanceof String) {
+            return asList(targetValue, otherValue);
+        } else {
+            // Assuming otherValue is a Collection
+            return addToBeginningOfCollection(targetValue, (Collection<String>) otherValue);
+        }
+    }
+
+    /**
+     * Adds element to the beginning of the collection
+     */
+    private List<String> addToBeginningOfCollection(String element, Collection<String> collection) {
         List<String> mergedList = new ArrayList<>();
-        mergedList.add(string);
+        mergedList.add(element);
         mergedList.addAll(collection);
         return mergedList;
     }
 
-    private List<String> mergeElementWithCollection(String string, Collection<String> collection) {
+    private List<String> addToEndOfCollection(Collection<String> collection, String element) {
         List<String> mergedList = new ArrayList<>(collection);
-        mergedList.add(string);
+        mergedList.add(element);
         return mergedList;
     }
 
-    @SuppressWarnings("unchecked")
-    private Object mergeMapKeyAsList(Map<String, Object> mergedMap, Map<String, Object> otherMap, String key) {
-        final Collection<String> mergedMapValue = (Collection<String>) mergedMap.get(key);
-        final Collection<String> otherMapValue = (Collection<String>) otherMap.get(key);
-        return Stream.concat(mergedMapValue.stream(), otherMapValue.stream())
+    private List<String> mergeListsOfStrings(Collection<String> targetValue, Collection<String> otherValue) {
+        return Stream.concat(targetValue.stream(), otherValue.stream())
                 .distinct()
                 .collect(toList());
-    }
-
-    @SuppressWarnings("unchecked")
-    private Map<String, Object> mergeMapKeyAsMap(Map<String, Object> mergedMap, Map<String, Object> otherMap,
-                                                 String key) {
-        final Map<String, Object> mergedMapValue = (Map<String, Object>) mergedMap.get(key);
-        final Map<String, Object> otherMapValue = (Map<String, Object>) otherMap.get(key);
-        return merge(Arrays.asList(mergedMapValue, otherMapValue));
     }
 }
--- src/test/java/dev/codesoapbox/dummy4j/definitions/providers/files/yaml/MapMergerTest.java
+++ src/test/java/dev/codesoapbox/dummy4j/definitions/providers/files/yaml/MapMergerTest.java
@@ -4,10 +4,11 @@ import dev.codesoapbox.dummy4j.definitions.providers.MapMerger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MapMergerTest {
@@ -20,24 +21,51 @@ class MapMergerTest {
     }
 
     @Test
-    void shouldMerge() {
-        Map<String, Object> yamlMap1 = new HashMap<>();
-        Map<String, Object> yamlMapRoot1 = new HashMap<>();
-        yamlMapRoot1.put("root1", "val1");
-        yamlMap1.put("en", yamlMapRoot1);
+    void shouldNotMutateOriginalMaps() {
+        List<Map<String, Object>> maps = advancedMapList();
 
-        Map<String, Object> yamlMap2 = new HashMap<>();
-        Map<String, Object> yamlMapRoot2 = new HashMap<>();
-        yamlMapRoot2.put("root2", "val2");
-        yamlMap2.put("en", yamlMapRoot2);
+        mapMerger.merge(maps);
+
+        assertEquals(advancedMapList(), maps);
+    }
+
+    private List<Map<String, Object>> advancedMapList() {
+        return asList(
+                simpleMap("pl", "root1", "val1"),
+                simpleMap("en", "root1", "val1"),
+                simpleMap("en", "root1", "val2"),
+                simpleMap("en", "root2", asList("val1", "val2")),
+                simpleMap("en", "root2", "val3")
+        );
+    }
+
+    /**
+     * {
+     * val1: {
+     * val2: val3
+     * }
+     * }
+     */
+    private Map<String, Object> simpleMap(String key1, String key2, Object val2) {
+        Map<String, Object> mapRoot = new HashMap<>();
+        Map<String, Object> mapSub = new HashMap<>();
+        mapRoot.put(key1, mapSub);
+        mapSub.put(key2, val2);
+        return mapRoot;
+    }
+
+    @Test
+    void shouldMerge() {
+        Map<String, Object> yamlMap1 = simpleMap("en", "root1", "val1");
+        Map<String, Object> yamlMap2 = simpleMap("en", "root2", "val2");
 
         Map<String, Object> expectedMap = new HashMap<>();
-        Map<String, Object> expectedRootMap = new HashMap<>();
-        expectedRootMap.put("root1", "val1");
-        expectedRootMap.put("root2", "val2");
-        expectedMap.put("en", expectedRootMap);
+        Map<String, Object> expectedSubMap = new HashMap<>();
+        expectedSubMap.put("root1", "val1");
+        expectedSubMap.put("root2", "val2");
+        expectedMap.put("en", expectedSubMap);
 
-        Map<String, Object> result = mapMerger.merge(Arrays.asList(yamlMap1, yamlMap2));
+        Map<String, Object> result = mapMerger.merge(asList(yamlMap1, yamlMap2));
         assertEquals(expectedMap, result);
     }
 
@@ -67,21 +95,14 @@ class MapMergerTest {
         expectedRootMap.put("root2", "val3");
         expectedMap.put("en", expectedRootMap);
 
-        Map<String, Object> result = mapMerger.merge(Arrays.asList(yamlMap1, yamlMap2));
+        Map<String, Object> result = mapMerger.merge(asList(yamlMap1, yamlMap2));
         assertEquals(expectedMap, result);
     }
 
     @Test
     void shouldNotMergeDifferentLocales() {
-        Map<String, Object> yamlMap1 = new HashMap<>();
-        Map<String, Object> yamlMapRoot1 = new HashMap<>();
-        yamlMapRoot1.put("root1", "val1");
-        yamlMap1.put("en", yamlMapRoot1);
-
-        Map<String, Object> yamlMap2 = new HashMap<>();
-        Map<String, Object> yamlMapRoot2 = new HashMap<>();
-        yamlMapRoot2.put("root1", "val1");
-        yamlMap2.put("pl", yamlMapRoot2);
+        Map<String, Object> yamlMap1 = simpleMap("en", "root1", "val1");
+        Map<String, Object> yamlMap2 = simpleMap("pl", "root1", "val1");
 
         Map<String, Object> expectedMap = new HashMap<>();
         Map<String, Object> expectedRootMap = new HashMap<>();
@@ -89,91 +110,51 @@ class MapMergerTest {
         expectedMap.put("en", expectedRootMap);
         expectedMap.put("pl", expectedRootMap);
 
-        Map<String, Object> result = mapMerger.merge(Arrays.asList(yamlMap1, yamlMap2));
+        Map<String, Object> result = mapMerger.merge(asList(yamlMap1, yamlMap2));
         assertEquals(expectedMap, result);
     }
 
     @Test
     void shouldMergeTwoLists() {
-        Map<String, Object> yamlMap1 = new HashMap<>();
-        Map<String, Object> yamlMapRoot1 = new HashMap<>();
-        yamlMapRoot1.put("root1", Arrays.asList("val1", "val2"));
-        yamlMap1.put("en", yamlMapRoot1);
+        Map<String, Object> yamlMap1 = simpleMap("en", "root1", asList("val1", "val2"));
+        Map<String, Object> yamlMap2 = simpleMap("en", "root1", asList("val3", "val4"));
+        Map<String, Object> expectedMap = simpleMap("en", "root1", asList("val1", "val2", "val3", "val4"));
 
-        Map<String, Object> yamlMap2 = new HashMap<>();
-        Map<String, Object> yamlMapRoot2 = new HashMap<>();
-        yamlMapRoot2.put("root1", Arrays.asList("val3", "val4"));
-        yamlMap2.put("en", yamlMapRoot2);
+        Map<String, Object> result = mapMerger.merge(asList(yamlMap1, yamlMap2));
 
-        Map<String, Object> expectedMap = new HashMap<>();
-        Map<String, Object> expectedRootMap = new HashMap<>();
-        expectedRootMap.put("root1", Arrays.asList("val1", "val2", "val3", "val4"));
-        expectedMap.put("en", expectedRootMap);
-
-        Map<String, Object> result = mapMerger.merge(Arrays.asList(yamlMap1, yamlMap2));
         assertEquals(expectedMap, result);
     }
 
     @Test
     void shouldMergeTwoSingleElements() {
-        Map<String, Object> yamlMap1 = new HashMap<>();
-        Map<String, Object> yamlMapRoot1 = new HashMap<>();
-        yamlMapRoot1.put("root1", "val1");
-        yamlMap1.put("en", yamlMapRoot1);
+        Map<String, Object> yamlMap1 = simpleMap("en", "root1", "val1");
+        Map<String, Object> yamlMap2 = simpleMap("en", "root1", "val2");
+        Map<String, Object> expectedMap = simpleMap("en", "root1", asList("val1", "val2"));
 
-        Map<String, Object> yamlMap2 = new HashMap<>();
-        Map<String, Object> yamlMapRoot2 = new HashMap<>();
-        yamlMapRoot2.put("root1", "val2");
-        yamlMap2.put("en", yamlMapRoot2);
+        Map<String, Object> result = mapMerger.merge(asList(yamlMap1, yamlMap2));
 
-        Map<String, Object> expectedMap = new HashMap<>();
-        Map<String, Object> expectedRootMap = new HashMap<>();
-        expectedRootMap.put("root1", Arrays.asList("val1", "val2"));
-        expectedMap.put("en", expectedRootMap);
-
-        Map<String, Object> result = mapMerger.merge(Arrays.asList(yamlMap1, yamlMap2));
         assertEquals(expectedMap, result);
     }
 
     @Test
     void shouldMergeListAndSingleElement() {
-        Map<String, Object> yamlMap1 = new HashMap<>();
-        Map<String, Object> yamlMapRoot1 = new HashMap<>();
-        yamlMapRoot1.put("root1", Arrays.asList("val1", "val2"));
-        yamlMap1.put("en", yamlMapRoot1);
+        Map<String, Object> yamlMap1 = simpleMap("en", "root1", asList("val1", "val2"));
+        Map<String, Object> yamlMap2 = simpleMap("en", "root1", "val3");
+        Map<String, Object> expectedMap = simpleMap("en", "root1", asList("val1", "val2", "val3"));
 
-        Map<String, Object> yamlMap2 = new HashMap<>();
-        Map<String, Object> yamlMapRoot2 = new HashMap<>();
-        yamlMapRoot2.put("root1", "val3");
-        yamlMap2.put("en", yamlMapRoot2);
+        Map<String, Object> result = mapMerger.merge(asList(yamlMap1, yamlMap2));
 
-        Map<String, Object> expectedMap = new HashMap<>();
-        Map<String, Object> expectedRootMap = new HashMap<>();
-        expectedRootMap.put("root1", Arrays.asList("val1", "val2", "val3"));
-        expectedMap.put("en", expectedRootMap);
-
-        Map<String, Object> result = mapMerger.merge(Arrays.asList(yamlMap1, yamlMap2));
         assertEquals(expectedMap, result);
     }
 
     @Test
     void shouldMergeSingleElementAndList() {
-        Map<String, Object> yamlMap1 = new HashMap<>();
-        Map<String, Object> yamlMapRoot1 = new HashMap<>();
-        yamlMapRoot1.put("root1", "val1");
-        yamlMap1.put("en", yamlMapRoot1);
+        Map<String, Object> yamlMap1 = simpleMap("en", "root1", "val1");
+        Map<String, Object> yamlMap2 = simpleMap("en", "root1", asList("val2", "val3"));
+        Map<String, Object> expectedMap = simpleMap("en", "root1", asList("val1", "val2", "val3"));
 
-        Map<String, Object> yamlMap2 = new HashMap<>();
-        Map<String, Object> yamlMapRoot2 = new HashMap<>();
-        yamlMapRoot2.put("root1", Arrays.asList("val2", "val3"));
-        yamlMap2.put("en", yamlMapRoot2);
+        Map<String, Object> result = mapMerger.merge(asList(yamlMap1, yamlMap2));
 
-        Map<String, Object> expectedMap = new HashMap<>();
-        Map<String, Object> expectedRootMap = new HashMap<>();
-        expectedRootMap.put("root1", Arrays.asList("val1", "val2", "val3"));
-        expectedMap.put("en", expectedRootMap);
-
-        Map<String, Object> result = mapMerger.merge(Arrays.asList(yamlMap1, yamlMap2));
         assertEquals(expectedMap, result);
     }
 }
