bba7854057f6aa43b7f35b21addaa881d5483781
Daniel Frąk <dan.frak@gmail.com>
2020-07-28 15:41:22 +0200

    Use EqualsVerifier for LocalizedDummyDefinitionsMap contract

--- src/test/java/dev/codesoapbox/dummy4j/definitions/LocalizedDummyDefinitionsMapTest.java
+++ src/test/java/dev/codesoapbox/dummy4j/definitions/LocalizedDummyDefinitionsMapTest.java
@@ -1,5 +1,6 @@
 package dev.codesoapbox.dummy4j.definitions;
 
+import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static java.util.Collections.emptyList;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LocalizedDummyDefinitionsMapTest {
 
@@ -36,66 +37,10 @@ class LocalizedDummyDefinitionsMapTest {
     }
 
     @Test
-    void shouldEqual() {
-        LocalizedDummyDefinitionsMap dummyDefinitionsMap1 = buildSimpleDummyDefinitions();
-        LocalizedDummyDefinitionsMap dummyDefinitionsMap2 = buildSimpleDummyDefinitions();
-
-        assertEquals(dummyDefinitionsMap1, dummyDefinitionsMap2);
-    }
-
-    @Test
-    void shouldNotEqualWithNull() {
-        assertNotEquals(buildSimpleDummyDefinitions(), null);
-    }
-
-    @Test
-    void shouldNotEqualWithDifferentLocales() {
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("key", "val");
-        LocalizedDummyDefinitionsMap dummyDefinitionsMap1 = new LocalizedDummyDefinitionsMap("en", map);
-        LocalizedDummyDefinitionsMap dummyDefinitionsMap2 = new LocalizedDummyDefinitionsMap("pl", map);
-
-        assertNotEquals(dummyDefinitionsMap1, dummyDefinitionsMap2);
-    }
-
-    @Test
-    void shouldNotEqualWithDifferentMaps() {
-        HashMap<String, Object> map1 = new HashMap<>();
-        map1.put("key", "val1");
-        HashMap<String, Object> map2 = new HashMap<>();
-        map2.put("key", "val2");
-        LocalizedDummyDefinitionsMap dummyDefinitionsMap1 = new LocalizedDummyDefinitionsMap("en", map1);
-        LocalizedDummyDefinitionsMap dummyDefinitionsMap2 = new LocalizedDummyDefinitionsMap("en", map2);
-
-        assertNotEquals(dummyDefinitionsMap1, dummyDefinitionsMap2);
-    }
-
-    @Test
-    void shouldNotEqualWithOtherClass() {
-        LocalizedDummyDefinitionsMap dummyDefinitionsMap = buildSimpleDummyDefinitions();
-
-        assertNotEquals(dummyDefinitionsMap, new Object());
-    }
-
-    @Test
-    void shouldEqualWithItself() {
-        LocalizedDummyDefinitionsMap dummyDefinitionsMap = buildSimpleDummyDefinitions();
-
-        assertEquals(dummyDefinitionsMap, dummyDefinitionsMap);
-    }
-
-    private LocalizedDummyDefinitionsMap buildSimpleDummyDefinitions() {
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("key", "val");
-        return new LocalizedDummyDefinitionsMap("en", map);
-    }
-
-    @Test
-    void shouldHaveSameHashcode() {
-        LocalizedDummyDefinitionsMap dummyDefinitionsMap1 = buildSimpleDummyDefinitions();
-        LocalizedDummyDefinitionsMap dummyDefinitionsMap2 = buildSimpleDummyDefinitions();
-
-        assertEquals(dummyDefinitionsMap1.hashCode(), dummyDefinitionsMap2.hashCode());
+    void equalsContract() {
+        EqualsVerifier.forClass(LocalizedDummyDefinitionsMap.class)
+                .usingGetClass()
+                .verify();
     }
 
     @Test
@@ -104,5 +49,11 @@ class LocalizedDummyDefinitionsMapTest {
 
         String expectedString = "LocalizedDummyDefinitions{locale='en', map={key=val}}";
         assertEquals(expectedString, dummyDefinitionsMap.toString());
+    }
+
+    private LocalizedDummyDefinitionsMap buildSimpleDummyDefinitions() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("key", "val");
+        return new LocalizedDummyDefinitionsMap("en", map);
     }
 }
